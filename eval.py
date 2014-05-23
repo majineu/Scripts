1e1a74810676373886f26138be05296ab3f576d8
majineu <majineu@outlook.com>
2014-05-23 11:19:33 +0800

    modify eval.py

--- eval.py
+++ eval.py
@@ -4,6 +4,8 @@ import re
 goldF = file(sys.argv[1])
 predF = file(sys.argv[2])
 
+# in branch 2
+
 for lines in zip(goldF, predF):
 	gold, pred = lines[0].strip(), lines[1].strip()
 	if len(gold) == 0:
